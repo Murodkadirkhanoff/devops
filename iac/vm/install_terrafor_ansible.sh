b6f3dd5143642b2bd6ae8e5fc357148f597a547c
Murodxon N. Qodirxonov <m_qodirxonov@uzinfocom.local>
2024-12-05 09:42:20 +0500

    first commit

---
+++ iac/vm/install_terrafor_ansible.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+
+# Terraform
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+
+sudo apt-get install terraform
+
+terraform -help
+
+
+# Ansible
+sudo apt update
+sudo apt install --yes software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install --yes ansible
+ansible –version
